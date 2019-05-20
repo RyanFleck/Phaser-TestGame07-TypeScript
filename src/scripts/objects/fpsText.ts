764f1391cb53ea98c6d9001f1721719930b05a34
Ryan Fleck <ryan.fleck@protonmail.com>
2019-05-20 14:01:03 -0400

    Remove phaser version text from main scenes.

--- src/scripts/objects/fpsText.ts
+++ src/scripts/objects/fpsText.ts
@@ -6,6 +6,6 @@ export default class FpsText extends Phaser.GameObjects.Text {
   }
 
   public update(scene: Phaser.Scene) {
-    this.setText(`${Math.floor(scene.game.loop.actualFps)} FPS`)
+    this.setText(Math.floor(scene.game.loop.actualFps).toString());
   }
 }
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -13,13 +13,6 @@ export default class MainScene extends Phaser.Scene {
     new PhaserLogo(this, this.cameras.main.width / 2, 0)
     this.fpsText = new FpsText(this)
 
-    // display the Phaser.VERSION
-    this.add
-      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
-        color: '#000000',
-        fontSize: 24
-      })
-      .setOrigin(1, 0)
   }
 
   update() {
